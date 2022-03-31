2dc53c7a171acb0983f883b255fbd20e51ff58a4
Bogibek_Matyaqubov <uzroot.pdp@gmail.com>
2022-03-31 19:21:54 +0500

    toast added

--- app/src/main/java/dev/matyaqubov/services/receivers/BootCompletedIntentReceiver.kt
+++ app/src/main/java/dev/matyaqubov/services/receivers/BootCompletedIntentReceiver.kt
@@ -1,16 +1,16 @@
 package dev.matyaqubov.services.receivers
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.widget.Toast
 import dev.matyaqubov.services.service.NotifyingDailyService
 
-import android.content.Intent
-import android.util.Log
 
-
-class BootCompletedIntentReceiver :BaseReceiver() {
+class BootCompletedIntentReceiver : BaseReceiver() {
     override fun onRebooted(context: Context?, sms: String?) {
         Log.w("boot_broadcast_poc", "starting service...");
         context!!.startService(Intent(context, NotifyingDailyService::class.java))
+        Toast.makeText(context, sms, Toast.LENGTH_SHORT).show()
     }
 }
